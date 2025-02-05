739d85fe321f43a003e860f4dccc89e1060f775f
Luis Henrique Bonini Santiago <luishbsantiago@gmail.com>
2025-02-05 14:34:10 -0300

    blog-list view and serializer

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.apiOverview, name='home'),
+    path('blog-list/', views.blogList, name='blog-list'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 from django.http import HttpResponse
+from .models import Blog
+from .serializer import BlogSerializer
 
 # Create your views here.
-def home(request):
-  return HttpResponse('Hello, Django!')
+@api_view(['GET'])
+def apiOverview(request):
+  api_urls = {
+    'List': 'api/blog-list/',
+    'Detail View': 'api/blog-detail/<str:pk>/',
+    'Create': 'api/blog-create/',
+    'Update': 'api/blog-update/<str:pk>/',
+    'Delete': 'api/blog-delete/<str:pk>/',
+  }
+  return Response(api_urls)
+
+@api_view(['GET'])
+def blogList(request):
+  blog = Blog.objects.all()
+  serializer = BlogSerializer(blog, many=True)
+  return Response(serializer.data)
